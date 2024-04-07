b1b556192252ef6f0f4ee2fd790cb1c11e26b384
zaid <zaid.tab123@gmail.com>
2024-04-07 09:43:51 +0530

    fix addr

--- client/src/helpers/constants.ts
+++ client/src/helpers/constants.ts
@@ -1,6 +1,6 @@
 const constants = {
   MODULEADDR:
-    "0xfc1acad6ac59c8a1c44dd930e9a55f4d3a5ba5a560e59b27651d4a70649191bf??",
+    "0xfc1acad6ac59c8a1c44dd930e9a55f4d3a5ba5a560e59b27651d4a70649191bf",
 };
 
 export default constants;
